c0783a4a3d9f424f59dc865ccc2f35acfc408602
dhanshrigade <dhanashrigade29@gmail.com>
2024-08-03 14:22:05 +0530

    Add files via upload

---
+++ duplicate.sql
@@ -0,0 +1,7 @@
+create table seat(id int primary key,student varchar(20));
+insert into seat values(1,abbot);
+insert into seat values(2,doris);
+insert into seat values(3,emerson);
+insert into seat values(4,green);
+insert into seat values(5,jeames);
+update student set id=student,student=id go;
